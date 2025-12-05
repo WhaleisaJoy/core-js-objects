752f92ac103a195e96d0482c6504e17f061831ca
Whale Joy <afterlifelineonomega@gmail.com>
2025-12-05 23:28:56 +0300

    feat: implement removeProperties task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -55,8 +55,16 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copy = { ...obj };
+
+  Object.keys(obj).forEach((key) => {
+    if (keys.includes(key)) {
+      delete copy[key];
+    }
+  });
+
+  return copy;
 }
 
 /**
