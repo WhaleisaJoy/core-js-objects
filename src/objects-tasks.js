e0e41e744175cda5bbc493d6443b948533c03d5b
Whale Joy <afterlifelineonomega@gmail.com>
2025-12-08 02:05:58 +0300

    feat: implement group task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -299,8 +299,19 @@ function sortCitiesArray(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((acc, next) => {
+    const key = keySelector(next);
+    const value = valueSelector(next);
+
+    if (acc.has(key)) {
+      acc.get(key).push(value);
+    } else {
+      acc.set(key, [value]);
+    }
+
+    return acc;
+  }, new Map());
 }
 
 /**
